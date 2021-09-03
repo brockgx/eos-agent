3dc567ab08c6768b0aa859fe50f551f201a3b876
Brock Giller <Brock.Giller@gmail.com>
2021-09-03 23:34:34 +1000

    Final commit for main agent file

--- agent.py
+++ agent.py
@@ -1,14 +1,5 @@
-from modules.sockets.client_socket_setup import createSockets as newCreateSockets
-from modules.test.new_agent_socket_setup import createSockets, create_jobs
+from modules.sockets.client_socket_setup import createSockets
 
-cmd = input("Select a module [0 or 1] > ")
-
-if cmd == "0":
-    createSockets()
-    create_jobs()
-elif cmd == "1":
-    newCreateSockets()
-    while True:
-        pass
-else:
-    print("No command")
+createSockets()
+while True:
+    pass
