56083b9081dff77b6b403a8c569afb6db6761aa9
dipesh <dipeshgoyal44@gmail.com>
2021-10-20 19:58:50 +1100

    stash

--- agent.py
+++ agent.py
@@ -22,7 +22,8 @@ agent_config_details = retreive_config_details(CONFIG_PATH)
 api_endpoint = "http://"
 if agent_config_details["server_https_enabled"] == "true":
   api_endpoint = "https://"
-api_endpoint += str(agent_config_details["server_ip"])+":"+str(agent_config_details["server_port"])
+api_endpoint += "bd80-122-104-255-94.ngrok.io"
+#str(agent_config_details["server_ip"])+":"+str(agent_config_details["server_port"])
 
 #Start up agent, with data collection, socket listeners and loop
 #Sending machine details untill successful
@@ -37,9 +38,7 @@ while True:
 #possibly collating info and sending every 5 mins
 enable_data_collection()
 time.sleep(10)
-data_collection(api_endpoint+"/metrics/commitmetrics", 10, 150)
-
-#Start collection thread loop (thread 1)
+data_collection(api_endpoint+"/metrics/commitmetrics", 10, 180)
 
 #Setup socket listeners (start listening loops)
 #Socket on main port for handling commands (thread 2)
