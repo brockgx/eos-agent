add17400345ec5bc2901f0061a59d8737517d1af
Brock Giller <Brock.Giller@gmail.com>
2021-10-23 19:37:43 +1100

    Finalised agent logging and config

--- modules/utilities/logging_setup.py
+++ modules/utilities/logging_setup.py
@@ -9,12 +9,13 @@ else:
   LOG_FILE = UTILITIES_DIR + "/agent.log"
 
 FORMATTER    = logging.Formatter('[%(asctime)s] (%(filename)s|%(funcName)s|ln %(lineno)d) [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+FORMATTER_BASIC = logging.Formatter('[%(asctime)s] [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 STREAM_HANDLER = logging.StreamHandler()
 FILE_HANDLER = logging.FileHandler(LOG_FILE)
 LOG_LEVEL = logging.DEBUG
 
 FILE_HANDLER.setFormatter(FORMATTER)
-STREAM_HANDLER.setFormatter(FORMATTER)
+STREAM_HANDLER.setFormatter(FORMATTER_BASIC)
 
 agent_logger = logging.getLogger(__name__)
 agent_logger.setLevel(LOG_LEVEL)
