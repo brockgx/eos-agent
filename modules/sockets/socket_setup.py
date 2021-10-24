85d02b35d97b77c164d2fad5fc6034ee2e9777a8
dipesh <dipeshgoyal44@gmail.com>
2021-10-24 16:07:50 +1100

    command read-write

--- modules/sockets/socket_setup.py
+++ modules/sockets/socket_setup.py
@@ -2,6 +2,7 @@
 import socket, time, select, queue, json
 
 #Import application specific modules
+from ..utilities.logging_setup import agent_logger
 from ..utilities.agent_core import print_log_msg, create_new_thread
 from .data_transfer import sendSocketData, receiveSocketData
 
@@ -25,7 +26,7 @@ def setup_socket_listener(socket_ip, socket_port):
   new_socket = configure_socket(socket_ip, socket_port)
   #Then run main function   
   mainFunction(new_socket)
-  print_log_msg("Doing stuff on socket (" + str(socket_port) + ")")
+  agent_logger.info("Socket is now listening on ({},{}).".format(socket_ip, socket_port))
 
 #Function: Generate the server socket
 #Params:
@@ -38,14 +39,14 @@ def configure_socket(socket_ip, socket_port):
     agent_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     agent_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    print_log_msg("Binding socket to port: " + str(socket_port))
+    agent_logger.info("Binding socket to port: {}".format(socket_port))
     agent_socket.bind((socket_ip, socket_port))
     agent_socket.listen(5)
 
     return agent_socket
   except Exception as err_msg:
-    print_log_msg("[Error] Socket creation failed.")
-    print_log_msg("[Error Msg]: " + str(err_msg))
+    agent_logger.critical("Socket creation failed on port: {}".format(socket_port))
+    agent_logger.critical(err_msg)
     return False
 
 #Function: Accepting new connections to the sockets (could be combined with select)
@@ -68,34 +69,32 @@ def mainFunction(sock):
 
 
   while allSocketConnections:
-    print("\nWaiting for next event (" + str(sock.getsockname()[1]) + ")...")
+    agent_logger.info("Waiting for next socket event ({})...".format(sock.getsockname()[1]))
     readable, writable, exceptional = select.select(allSocketConnections, allSocketOutputs, allSocketConnections)     #Not working after this.
 
     for read in readable:
       if read is sock:
-        print("Accepting connnections")
         connection, client_address = read.accept()
+        agent_logger.info("New connection accepted from: {}.".format(connection.getpeername()))
         connection.setblocking(0)
         allSocketConnections.append(connection)
         allMessageQueues[connection] = queue.Queue()
       else:
         data = receiveSocketData(read)
         if data:
-          #json_data = json.loads(data)
-          print("Receieved: " + str(data) + " from (" + str(read.getpeername()) + ").")
-          if data == "fileupload":
-            print("fileupload")
-
-          elif data == "precommand":
-            print("precommand")
-            #run command function here
-            sendSocketData(read, data)
-          elif data == "command":
-            print("precommand")
+          json_data = json.loads(data)
+          agent_logger.info("Command data receieved {} from ({}).".format(data, read.getpeername()))
+          if json_data["type"] == "fileupload":
+            allMessageQueues[read].put(json_data["details"]["msg"])
+          elif json_data["type"] == "appshutdown":
+            agent_logger.info("Attempting to stop application with name: {} and PID: {}.".format(json_data["details"]["app_name"], json_data["details"]["app_pid"]))
+            allMessageQueues[read].put("Fuck you Dylan!!")
+            time.sleep(2)
+            agent_logger.info("Application with details: ({},{}) stopped.".format(json_data["details"]["app_name"], json_data["details"]["app_pid"]))
           if read not in allSocketOutputs:
             allSocketOutputs.append(read)
         else:
-          print("Closing " + str(read.getsockname()) + " after reading no data")
+          agent_logger.info("Closing {} after reading no data.".format(read.getpeername()))
           if read in allSocketOutputs:
             allSocketOutputs.remove(read)
           
@@ -106,26 +105,17 @@ def mainFunction(sock):
       try:
         next_msg = allMessageQueues[write].get_nowait()
       except queue.Empty:
-        print("Output queue for " + str(write.getpeername()) + " is empty")
+        #agent_logger.warning("Output queue for {} is empty.".format(write.getpeername()))
         allSocketOutputs.remove(write)
       else:
-        if data == "precommand":
-          reply = "precommand received"
-          noreply = "didnt receive"
-          print("Sending " + str(reply) + " to " + str(write.getpeername()))
-          sendSocketData(write, reply)
-          allSocketConnections.remove(write) #removing the connection from list after sending
-        else:
-          print("Sending " + str(noreply) + " to " + str(write.getpeername()))
-          sendSocketData(write, noreply)
-          allSocketConnections.remove(write) #removing the connection from list after sending
-
+        #agent_logger.info("Sending {} to {}.".format(next_msg, write.getpeername()))
+        sendSocketData(write, next_msg)
+        allSocketConnections.remove(write) #removing the connection from list after sending
 
     for exc in exceptional:
-      print("Handling exceptional condition for " + str(exc.getpeername()))
+      agent_logger.info("Handling exceptional condition for {}.".format(exc.getpeername()))
       allSocketConnections.remove(exc)
       if exc in allSocketOutputs:
         allSocketOutputs.remove(exc)
       exc.close()
       del allMessageQueues[exc]
-
