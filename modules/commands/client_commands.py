e4683382ca9db72ca7c85ef4b5639d6f45c28368
dipesh <dipeshgoyal44@gmail.com>
2021-10-27 18:53:56 +1100

    agent client_command

--- modules/commands/client_commands.py
+++ modules/commands/client_commands.py
@@ -2,21 +2,48 @@ import os
 import json
 import base64
 import subprocess
+import time
+from ..utilities.logging_setup import agent_logger
 
 def jsonProcessor(json):
     type = json["type"]
     params = json["parameters"]
     if json["type"] == "fileupload":
+        print("fileupload Received")
+        agent_logger.info("Attempting to upload the file at destination: {}.".format(params["destination"]))
         return fileProcessor(params)
     elif json["type"] == "appshutdown":
-        return "killapp"
-    elif json["type"] == "command":
-        return commandProcessor(params)
-    elif json["type"] == "shell":
+        print("appshutdown Received")
+        agent_logger.info("Attempting to stop application with name: {} and PID: {}.".format(params["app_name"], params["app_id"]))
+        return "appshutdown"
+       #agent_logger.info("Application with details: ({},{}) stopped.".format(params["app_name"], params["app_id"]))     
+    elif json["type"] == "restartapp":
+        print("appshutdown Received")
+        agent_logger.info("Attempting to restart application with name: {} and PID: {}.".format(params["app_name"], params["app_id"]))
+        return "restartapp"
+       #agent_logger.info("Application with details: ({},{}) restarted.".format(params["app_name"], params["app_id"]))    
+    elif json["type"] == "shutdownmachine":
+        print("shutdownmachine Received")
+        agent_logger.info("Attempting to shutdown machine {} .".format(json["machine_name"]))
+        return shutdown(json)
+    elif json["type"] == "restartmachine":
+        print("restartmachine Received")
+        agent_logger.info("Attempting to restart machine {} .".format(json["machine_name"]))
+        return restart_command(json)
+    elif json["type"] == "custom_command":
+        print("custom Received")
+        agent_logger.info("Attempting to run the command: {} on machine: {}.".format(params["custom_command"], params["machine_name"]))
         return shellProcessor(params)
-
-def shutdown():
+          
+def shutdown(json):
     os.system("shutdown /s /t 0")
+    agent_logger.info("Machine Shutdown: {} .".format(json["machine_name"]))
+    return "Shutdown Initiated."
+
+def restart_command(json):
+    print("JSON Command Processor")
+    agent_logger.info("Restarted machine {} .".format(json["machine_name"]))
+    return "Reset Initiated."
     
 def fileProcessor(params):
     print("File Processor")
@@ -26,28 +53,17 @@ def fileProcessor(params):
     destination = params['destination']
     print("File Received")
     #print(b64file)
-    file = base64.b64decode(b64file)
-    outFileHandle = open(destination, "wb")
-    outFileHandle.write(file)
+    # file = base64.b64decode(b64file)
+    # outFileHandle = open(destination, "wb")
+    # outFileHandle.write(file)
     result = "File Written to " + destination
     print(result)
+    agent_logger.info("File: uploaded at destination: {}.".format(destination))
     return result
 
-def commandProcessor(params):
-    print("JSON Command Processor")
-    #print(commandJson)
-    command = params['command']
-    if command == "shutdown":
-        print("Shutdown Initiated")
-        return "Shutdown Initiated."
-        #shutdown()
-    elif command == "reset":
-        print("Reset Initiated")
-        return "Reset Initiated."
-
-def shellProcessor(params):
-  print("Shell command")
-  #print(commandJson)
+def shellProcessor(json):
+  print("Custom command")
+  params = json["parameters"]
   command = params['command']
   if type == "shell":
     print("Shell Command Received")
@@ -61,4 +77,5 @@ def shellProcessor(params):
       left = command
     result = subprocess.run([left, right], capture_output=True).stdout.decode('utf-8')
     print(result)
+    agent_logger.info("Custom Command ran sucessfully on machine {}.".format(json["machine_name"]))
     return result
